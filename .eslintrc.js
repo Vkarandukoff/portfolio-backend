ae78acf37c98c7440e30c028d4415fecacd7226f
Volodymyr Karandukov <vkarandukoff2018@gmail.com>
2023-03-17 10:43:53 +0200

    eslint init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
