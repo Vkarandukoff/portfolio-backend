5dd3deae54dfc7a6baefd49052b2cdfd575fcb8c
Volodymyr Karandukov <vkarandukoff2018@gmail.com>
2023-03-31 16:55:08 +0300

    test pipeline

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 
-console.log('Opa pipeline chinazes');
+console.log('Hello ');
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
