8915362c224dd3a7ac85e133f7d4ae2cf5f606d4
Volodymyr Karandukov <vkarandukoff2018@gmail.com>
2023-03-31 14:16:56 +0300

    added log for env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import * as process from 'process';
+
+console.log(process.env);
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
